7a03f2347381f10fb5926c58abc616722a91f50d
Henry Liou <1654183+liouh@users.noreply.github.com>
2026-09-26 10:32:56 -0700

    Update buildingMappings.ts

--- src/lib/buildingMappings.ts
+++ src/lib/buildingMappings.ts
@@ -523,7 +523,7 @@ export const getBuildingIcon = (name: string): string | null => {
   let icon: string | null = null;
 
   if (name.includes("Farm") || name.includes("Pasture")) icon = "";
-  else if (name.includes("Palisade Wall")) icon = "";
+  else if (name.includes("House")) icon = "";
   else if (name.includes("Wall")) icon = "";
   else if (name.includes("Archery Range")) icon = "🏹";
   else if (name.includes("Barracks")) icon = "⚔️";
@@ -533,7 +533,6 @@ export const getBuildingIcon = (name: string): string | null => {
   else if (name.includes("Feitoria") || name.includes("Caravanserai")) icon = "🏛️";
   else if (name.includes("Fish")) icon = "🐟";
   else if (name.includes("Gate")) icon = "⛩️";
-  else if (name.includes("House") || name.includes("Tent")) icon = "";
   else if (name.includes("Lumber Camp")) icon = "🌲\uFE0E";
   else if (name.includes("Market")) icon = "⚖️";
   else if (name.includes("Mill") || name.includes("Folwark")) icon = "𖣘";
@@ -545,9 +544,11 @@ export const getBuildingIcon = (name: string): string | null => {
   else if (name.includes("Siege Workshop")) icon = "⚙️";
   else if (name.includes("Stable")) icon = "🐎"; //🐴
   else if (name.includes("Tower") || name.includes("Donjon") || name.includes("Krepost") || name.includes("Fort")) icon = "♜";
-  else if (name.includes("Town Center")) icon = "🏫";
+  else if (name.includes("Town Center")) icon = "🏠";
+  else if (name.includes("Trade Workshop")) icon = "✨";
   else if (name.includes("University")) icon = "📖\uFE0E";
   else if (name.includes("Wonder")) icon = "⭐";
+  else if (name.includes("Yurt") || name.includes("Tent")) icon = "⛺";
   else icon = "❓";
 
   return icon;
