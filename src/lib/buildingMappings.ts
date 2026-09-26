85b8b739e94e0aecdebf34ec974d26b0c2f74ddf
Henry Liou <1654183+liouh@users.noreply.github.com>
2026-09-26 06:30:54 -0700

    Add building icon for fish traps

--- src/lib/buildingMappings.ts
+++ src/lib/buildingMappings.ts
@@ -531,6 +531,7 @@ export const getBuildingIcon = (name: string): string | null => {
   else if (name.includes("Castle")) icon = "🏰";
   else if (name.includes("Dock") || name.includes("Harbor") || name.includes("Shipyard") || name.includes("Port")) icon = "⚓";
   else if (name.includes("Feitoria") || name.includes("Caravanserai")) icon = "🏛️";
+  else if (name.includes("Fish")) icon = "🐟";
   else if (name.includes("Gate")) icon = "⛩️";
   else if (name.includes("House") || name.includes("Tent")) icon = "";
   else if (name.includes("Lumber Camp")) icon = "🌲\uFE0E";
